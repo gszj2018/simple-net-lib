e96d492d5ac4acb5f0afc26e2a16d735fd192896
GS <gsxj2014@163.com>
2022-10-05 15:54:11 +0800

    fix a potential race condition

--- src/io_context.cpp
+++ src/io_context.cpp
@@ -196,8 +196,16 @@ EventPoller::EventPoller(EventQueue *q, int pollSize) :
 
 
 void EventPoller::registerEvent(EventRegOp op, int fd, EventType type, std::shared_ptr<EventObject> ptr) {
-    if (closed_.load(std::memory_order::acquire)) {
-        return;
+    if (op != EventRegOp::REARM) {
+        std::lock_guard<std::recursive_mutex> lock(mapMutex_);
+        if (closed_.load(std::memory_order::acquire)) return;
+        if (op == EventRegOp::REGISTER) {
+            fdMap_[fd] = std::move(ptr);
+        } else if (op == EventRegOp::DEREGISTER) {
+            fdMap_.erase(fd);
+        }
+    } else {
+        if (closed_.load(std::memory_order::acquire)) return;
     }
     constexpr int EPOLL_OP_MAP[] = {EPOLL_CTL_ADD, EPOLL_CTL_MOD, EPOLL_CTL_DEL};
     int epOp = EPOLL_OP_MAP[static_cast<int>(op)];
@@ -207,14 +215,6 @@ void EventPoller::registerEvent(EventRegOp op, int fd, EventType type, std::shar
     ev.events = EPOLLONESHOT;
     ev.events |= (type & EVENT_IN) ? EPOLLIN : 0;
     ev.events |= (type & EVENT_OUT) ? EPOLLOUT : 0;
-    if (op != EventRegOp::REARM) {
-        std::lock_guard<std::recursive_mutex> lock(mapMutex_);
-        if (op == EventRegOp::REGISTER) {
-            fdMap_[fd] = std::move(ptr);
-        } else if (op == EventRegOp::DEREGISTER) {
-            fdMap_.erase(fd);
-        }
-    }
     if (epoll_ctl(epfd_, epOp, fd, &ev) < 0) {
         Logger::global->log(LOG_WARN, strerror(errno));
     }
